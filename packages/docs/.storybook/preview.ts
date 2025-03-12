6c7308c5e5822cc4f12254c82c89c52545a294fa
Pamela Santos <pamesantoss@hotmail.com>
2025-03-12 18:42:29 +0000

    chore: configura tema dark para os docs e preview

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,7 +1,18 @@
 import type { Preview } from '@storybook/react'
+import { themes } from '@storybook/theming'
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      values: [
+        { name: 'Dark', value: '#333' },
+        { name: 'Light', value: '#F7F9F2' },
+      ],
+      default: 'Dark',
+    },
+    docs: {
+      theme: themes.dark
+    },
     controls: {
       matchers: {
        color: /(background|color)$/i,
