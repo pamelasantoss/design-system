2dacd423e342ff9355daf1a24e1650bd8dd357f7
Pamela Santos <pamesantoss@hotmail.com>
2025-03-16 15:30:15 -0300

    feat: criando o componente Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import type { StoryObj, Meta } from "@storybook/react";
+import { Text, TextProps } from "@ignite-ui/react";
+
+export default {
+  title: "Typography/Text",
+  component: Text,
+
+  args: {
+    children:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac tempus diam. In nec maximus enim.",
+  },
+} as Meta<TextProps>;
+
+export const Primary: StoryObj<TextProps> = {};
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: "Strong text",
+    as: "strong",
+  },
+};
