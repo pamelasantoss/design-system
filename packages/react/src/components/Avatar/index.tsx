a58415da75b0546fecf831106d3812934ecbd5e9
Pamela Santos <pamesantoss@hotmail.com>
2025-03-16 16:17:55 -0300

    feat: criação do componente Avatar

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,17 @@
+import { ComponentProps } from 'react'
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
+import { User } from 'phosphor-react'
+
+export function Avatar() {
+  return (
+    <AvatarContainer>
+      <AvatarImage />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
